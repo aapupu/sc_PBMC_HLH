a792b87326a4dc7b0a62a186e991f652437b6270
aapupu <94039288+aapupu@users.noreply.github.com>
2024-10-14 10:33:56 +0800

    Update cellranger.sh

--- code/cellranger.sh
+++ code/cellranger.sh
@@ -1,2 +1,2 @@
-nohup cellranger count --id=F012 --fastqs=/F012/RNA --transcriptome=/data4/laiwp/EBVref/EBV --sample=F012 --localcores=8 --localmem=64 --chemistry SC5P-PE &
-nohup cellranger count --id=P1-1 --fastqs=/P1-1 --transcriptome=/data4/laiwp/EBVref/EBV --sample=P1-1 --localcores=8 --localmem=64 & 
+nohup cellranger count --id=F012 --fastqs=/F012/RNA --transcriptome=/EBVref/EBV --sample=F012 --localcores=8 --localmem=64 --chemistry SC5P-PE &
+nohup cellranger count --id=P1-1 --fastqs=/P1-1 --transcriptome=/EBVref/EBV --sample=P1-1 --localcores=8 --localmem=64 & 
